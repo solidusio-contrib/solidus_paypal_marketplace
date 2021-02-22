b7dc266ce7b538eb2cc6cb2e3ea063eaa1261213
Giacomo Bertoldi <bertoldi.giacomo@gmail.com>
2021-02-22 14:24:37 +0100

    add seller form

--- app/controllers/spree/admin/sellers_controller.rb
+++ app/controllers/spree/admin/sellers_controller.rb
@@ -1,7 +1,19 @@
 
 module Spree
   module Admin
-    class SellersController < Spree::Admin::BaseController
+    class SellersController < Spree::Admin::ResourceController
+      before_action :set_merchant_id,
+                    :set_action_url, only: [:create]
+
+      private
+
+      def set_action_url
+        @seller.action_url = 'url'
+      end
+
+      def set_merchant_id
+        @seller.merchant_id = SecureRandom.uuid
+      end
     end
   end
 end

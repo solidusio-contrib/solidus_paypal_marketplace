59aabbdaef08b043c88122cb8eac0a3a5889dab3
Alessandro <descovi@gmail.com>
2021-02-23 17:21:36 +0100

    add seller controller index action

--- app/controllers/spree/admin/sellers_controller.rb
+++ app/controllers/spree/admin/sellers_controller.rb
@@ -3,7 +3,20 @@
 module Spree
   module Admin
     class SellersController < Spree::Admin::ResourceController
+      def index
+        session[:return_to] = request.url
+        respond_with(@collection)
+      end
 
+      private
+
+      def collection
+        return @collection if @collection.present?
+
+        @collection = super
+        @search = @collection.ransack(params[:q])
+        @collection = @search.result.page(params[:page])
+      end
     end
   end
 end

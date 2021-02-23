2654c2ee3fbe04a539f3b9ea7d3646dde614c649
Alessandro <descovi@gmail.com>
2021-02-23 15:28:08 +0100

    :construction: add seller merchant_id inside before create callback

--- app/controllers/spree/admin/sellers_controller.rb
+++ app/controllers/spree/admin/sellers_controller.rb
@@ -3,20 +3,7 @@
 module Spree
   module Admin
     class SellersController < Spree::Admin::ResourceController
-      before_action :set_merchant_id, :set_action_url, only: [:create]
 
-      private
-
-      def set_action_url
-        @seller.action_url = ''
-      end
-
-      def set_merchant_id
-        @seller.merchant_id = Digest::UUID.uuid_v5(
-          Digest::UUID::DNS_NAMESPACE,
-          request.host
-        )
-      end
     end
   end
 end
--- app/models/spree/seller.rb
+++ app/models/spree/seller.rb
@@ -10,5 +10,11 @@ module Spree
 
     enum status: { pending: 0, accepted: 1, rejected: 2 }
     enum risk_status: { subscribed: 0, subscribed_with_limit: 1, declined: 2, manual_review: 3, need_more_data: 4 }
+
+    before_create :set_merchant_id
+
+    def set_merchant_id
+      self.merchant_id = SecureRandom.uuid
+    end
   end
 end

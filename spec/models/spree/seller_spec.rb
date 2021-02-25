2da9d9439366f37d4800dfda2c47c95cf044f3c7
Alessandro <descovi@gmail.com>
2021-02-25 12:10:27 +0100

    Fix test: add seller object inside expectation method.

--- spec/models/spree/seller_spec.rb
+++ spec/models/spree/seller_spec.rb
@@ -28,11 +28,11 @@ RSpec.describe Spree::Seller, type: :model do
     subject(:seller) { described_class.create!(name: 'Mr Good') }
 
     it {
-      seller.to validate_numericality_of(:percentage).is_greater_than_or_equal_to(0)
+      expect(seller).to validate_numericality_of(:percentage).is_greater_than_or_equal_to(0)
     }
 
     it {
-      seller.to validate_numericality_of(:percentage).is_less_than_or_equal_to(100)
+      expect(seller).to validate_numericality_of(:percentage).is_less_than_or_equal_to(100)
     }
   end
 end

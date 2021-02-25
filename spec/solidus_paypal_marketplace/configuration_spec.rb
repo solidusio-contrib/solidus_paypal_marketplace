4856834619b3906e53fe325800f17b87f39d5a53
Alessandro <descovi@gmail.com>
2021-02-25 17:01:44 +0100

    Add test for configuration.

---
+++ spec/solidus_paypal_marketplace/configuration_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe SolidusPaypalMarketplace::Configuration do
+  before do
+    SolidusPaypalMarketplace.configure do |config|
+      config.paypal_client_id = 'sb-user@business.example.com'
+      config.paypal_client_secret = 'secret-stuff'
+      config.partner_code = 'paypal-partner-code'
+    end
+  end
+
+  it 'read paypal_client_id value' do
+    expect(SolidusPaypalMarketplace.config.paypal_client_id).to be 'sb-user@business.example.com'
+  end
+
+  it 'read paypal_client_secret value' do
+    expect(SolidusPaypalMarketplace.config.paypal_client_secret).to be 'secret-stuff'
+  end
+
+  it 'read partner_code value' do
+    expect(SolidusPaypalMarketplace.config.partner_code).to be 'paypal-partner-code'
+  end
+end

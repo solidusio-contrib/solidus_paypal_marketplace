361695d72391a949785d8eafd2413b7c8e0a27e9
Giacomo Bertoldi <bertoldi.giacomo@gmail.com>
2021-03-02 11:13:13 +0100

    add users association to sellers

--- spec/models/spree/seller_spec.rb
+++ spec/models/spree/seller_spec.rb
@@ -5,6 +5,7 @@ require 'spec_helper'
 RSpec.describe Spree::Seller, type: :model do
   it { is_expected.to be_kind_of Spree::SoftDeletable }
   it { is_expected.to define_enum_for(:status).with_values(pending: 0, accepted: 1, rejected: 2) }
+  it { is_expected.to have_many(:users).dependent(:destroy) }
   it { is_expected.to have_one(:stock_location).dependent(:destroy) }
 
   it do

f847c72b32dbe31d0db389d00829059435ee888f
Daniele Palombo <daniele.palombo@gmail.com>
2021-03-20 14:04:00 +0100

    Check seller price presence at line item creation
    When a line item is created (or added to the cart), a price
    for the related seller should exists.

--- app/decorators/add_seller_to_line_item_decorator.rb
+++ app/decorators/add_seller_to_line_item_decorator.rb
@@ -3,6 +3,14 @@
 module AddSellerToLineItemDecorator
   def self.prepended(base)
     base.belongs_to :seller, class_name: 'Spree::Seller'
+    base.validate :validate_seller_price_presence, if: -> { variant.present? }
+  end
+
+  private
+
+  def validate_seller_price_presence
+    pricing_options = Spree::Variant::PricingOptions.from_line_item(self)
+    errors.add(:seller, :no_prices) if variant.price_for_seller(seller, pricing_options).nil?
   end
 
   Spree::LineItem.prepend self
--- spec/decorators/add_seller_to_line_item_decorator_spec.rb
+++ spec/decorators/add_seller_to_line_item_decorator_spec.rb
@@ -10,4 +10,45 @@ RSpec.describe AddSellerToLineItemDecorator, type: :model do
       .to belong_to(:seller)
       .class_name('Spree::Seller')
   end
+
+  describe '#validate_seller_price_presence' do
+    subject(:valid?) { line_item.valid? }
+
+    let(:seller) { create(:seller) }
+    let(:price) { create(:price, seller: seller) }
+    let(:line_item) { build(:line_item, variant: price.variant, seller: seller) }
+
+    it 'is valid' do
+      expect(valid?).to be_truthy
+    end
+
+    context 'when there are no prices for variant' do
+      let(:variant) { create(:variant).tap { |v| v.prices.delete_all } }
+      let(:line_item) { build(:line_item, variant: variant, seller: seller) }
+
+      it 'is not valid' do
+        expect(line_item.variant.prices).to be_empty
+
+        expect(valid?).to be_falsey
+
+        expect(
+          line_item.errors.added?(:seller, :no_prices)
+        ).to be true
+      end
+    end
+
+    context 'when there are no prices for seller/variant combination' do
+      let(:price) { create(:price, seller: create(:seller, name: 'Other Seller')) }
+
+      it 'is not valid' do
+        expect(line_item.variant.prices.where(seller: seller)).to be_empty
+
+        expect(valid?).to be_falsey
+
+        expect(
+          line_item.errors.added?(:seller, :no_prices)
+        ).to be true
+      end
+    end
+  end
 end

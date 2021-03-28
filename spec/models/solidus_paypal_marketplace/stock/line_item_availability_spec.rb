38c18cf9e156efed662cca286d8c50f65028c526
Daniele Palombo <daniele.palombo@gmail.com>
2021-03-28 15:51:03 +0200

    FIx rubocop offenses

--- spec/models/solidus_paypal_marketplace/stock/line_item_availability_spec.rb
+++ spec/models/solidus_paypal_marketplace/stock/line_item_availability_spec.rb
@@ -2,141 +2,156 @@
 
 require 'spec_helper'
 
-module SolidusPaypalMarketplace::Stock
-  RSpec.describe LineItemAvailability do
-    let(:variants) { Spree::Variant.all.to_a }
-    let(:infinity) { Float::INFINITY }
+RSpec.describe SolidusPaypalMarketplace::Stock::LineItemAvailability do
+  subject(:availability!) { availability }
 
-    let(:availability) { described_class.new(variants: variants) }
+  let(:variants) { Spree::Variant.all.to_a }
+  let(:infinity) { Float::INFINITY }
 
-    let!(:stock_location1) { create(:stock_location) }
+  let(:availability) { described_class.new(variants: variants) }
 
-    subject { availability }
+  let!(:stock_location1) { create(:stock_location) }
 
-    describe "#on_hand_by_stock_location_id" do
-      subject { availability.on_hand_by_stock_location_id }
+  describe "#on_hand_by_stock_location_id" do
+    subject(:on_hand_by_stock_location_in!) { availability.on_hand_by_stock_location_id }
 
-      context 'with a single variant' do
-        let!(:variant) { create(:master_variant) }
-        let(:stock_item) { variant.stock_items[0] }
+    context 'with a single variant' do
+      let!(:variant) { create(:master_variant) }
+      let(:stock_item) { variant.stock_items[0] }
 
-        context 'with count_on_hand positive' do
-          before { stock_item.set_count_on_hand(2) }
+      context 'with count_on_hand positive' do
+        before { stock_item.set_count_on_hand(2) }
+
+        it "returns the correct value" do
+          expect(on_hand_by_stock_location_in!).to eq(
+            stock_location1.id => Spree::StockQuantities.new(variant => 2)
+          )
+        end
+
+        context 'when backorderable is false' do
+          before { stock_item.update!(backorderable: false) }
 
           it "returns the correct value" do
-            expect(subject).to eq(stock_location1.id => Spree::StockQuantities.new(variant => 2))
-          end
-
-          context 'and backorderable false' do
-            before { stock_item.update!(backorderable: false) }
-
-            it "returns the correct value" do
-              expect(subject).to eq(stock_location1.id => Spree::StockQuantities.new(variant => 2))
-            end
-          end
-        end
-
-        context 'with count_on_hand 0' do
-          before { stock_item.set_count_on_hand(0) }
-
-          it "returns zero on_hand" do
-            expect(subject).to eq(stock_location1.id => Spree::StockQuantities.new(variant => 0))
-          end
-        end
-
-        context 'with count_on_hand negative' do
-          before { stock_item.set_count_on_hand(-1) }
-
-          it "returns zero on_hand" do
-            expect(subject).to eq(stock_location1.id => Spree::StockQuantities.new(variant => 0))
-          end
-        end
-
-        context 'with no stock_item' do
-          before { stock_item.destroy! }
-
-          it "returns empty hash" do
-            expect(subject).to eq({})
-          end
-        end
-
-        context 'with soft-deleted stock_item' do
-          before { stock_item.discard }
-
-          it "returns empty hash" do
-            expect(subject).to eq({})
-          end
-        end
-
-        context 'with track_inventory=false' do
-          before { variant.update!(track_inventory: false) }
-
-          it "has infinite inventory " do
-            expect(subject).to eq(stock_location1.id => Spree::StockQuantities.new(variant => infinity))
-          end
-        end
-
-        context 'with config.track_inventory_levels=false' do
-          before { stub_spree_preferences(track_inventory_levels: false) }
-
-          it "has infinite inventory " do
-            expect(subject).to eq(stock_location1.id => Spree::StockQuantities.new(variant => infinity))
+            expect(on_hand_by_stock_location_in!).to eq(
+              stock_location1.id => Spree::StockQuantities.new(variant => 2)
+            )
           end
         end
       end
+
+      context 'with count_on_hand 0' do
+        before { stock_item.set_count_on_hand(0) }
+
+        it "returns zero on_hand" do
+          expect(on_hand_by_stock_location_in!).to eq(
+            stock_location1.id => Spree::StockQuantities.new(variant => 0)
+          )
+        end
+      end
+
+      context 'with count_on_hand negative' do
+        before { stock_item.set_count_on_hand(-1) }
+
+        it "returns zero on_hand" do
+          expect(on_hand_by_stock_location_in!).to eq(
+            stock_location1.id => Spree::StockQuantities.new(variant => 0)
+          )
+        end
+      end
+
+      context 'with no stock_item' do
+        before { stock_item.destroy! }
+
+        it "returns empty hash" do
+          expect(on_hand_by_stock_location_in!).to eq({})
+        end
+      end
+
+      context 'with soft-deleted stock_item' do
+        before { stock_item.discard }
+
+        it "returns empty hash" do
+          expect(on_hand_by_stock_location_in!).to eq({})
+        end
+      end
+
+      context 'with track_inventory=false' do
+        before { variant.update!(track_inventory: false) }
+
+        it "has infinite inventory " do
+          expect(on_hand_by_stock_location_in!).to eq(
+            stock_location1.id => Spree::StockQuantities.new(variant => infinity)
+          )
+        end
+      end
+
+      context 'with config.track_inventory_levels=false' do
+        before { stub_spree_preferences(track_inventory_levels: false) }
+
+        it "has infinite inventory " do
+          expect(on_hand_by_stock_location_in!).to eq(
+            stock_location1.id => Spree::StockQuantities.new(variant => infinity)
+          )
+        end
+      end
     end
+  end
 
-    describe "#backorderable_by_stock_location_id" do
-      subject { availability.backorderable_by_stock_location_id }
+  describe "#backorderable_by_stock_location_id" do
+    subject { availability.backorderable_by_stock_location_id }
 
-      context 'with a single variant' do
-        let!(:variant) { create(:master_variant) }
-        let(:stock_item) { variant.stock_items[0] }
+    context 'with a single variant' do
+      let!(:variant) { create(:master_variant) }
+      let(:stock_item) { variant.stock_items[0] }
 
-        context 'with backorderable false' do
-          before { stock_item.update!(backorderable: false) }
+      context 'with backorderable false' do
+        before { stock_item.update!(backorderable: false) }
 
-          context 'and positive count_on_hand' do
-            before { stock_item.set_count_on_hand(2) }
-            it { is_expected.to eq({}) }
-          end
-
-          context 'and 0 count_on_hand' do
-            before { stock_item.set_count_on_hand(0) }
-            it { is_expected.to eq({}) }
-          end
-        end
-
-        context 'with backorderable true' do
-          before { stock_item.update!(backorderable: true) }
-
-          context 'and positive count_on_hand' do
-            before { stock_item.set_count_on_hand(2) }
-            it { is_expected.to eq(stock_location1.id => Spree::StockQuantities.new(variant => infinity)) }
-          end
-
-          context 'and 0 count_on_hand' do
-            before { stock_item.set_count_on_hand(0) }
-            it { is_expected.to eq(stock_location1.id => Spree::StockQuantities.new(variant => infinity)) }
-          end
-
-          context 'and negative count_on_hand' do
-            before { stock_item.set_count_on_hand(-1) }
-            it { is_expected.to eq(stock_location1.id => Spree::StockQuantities.new(variant => infinity)) }
-          end
-        end
-
-        context 'with soft-deleted stock_item' do
-          before { stock_item.discard }
+        context 'with positive count_on_hand' do
+          before { stock_item.set_count_on_hand(2) }
 
           it { is_expected.to eq({}) }
         end
 
-        context 'with no stock_item' do
-          before { stock_item.destroy }
+        context 'with 0 count_on_hand' do
+          before { stock_item.set_count_on_hand(0) }
 
           it { is_expected.to eq({}) }
         end
+      end
+
+      context 'with backorderable true' do
+        before { stock_item.update!(backorderable: true) }
+
+        context 'with positive count_on_hand' do
+          before { stock_item.set_count_on_hand(2) }
+
+          it { is_expected.to eq(stock_location1.id => Spree::StockQuantities.new(variant => infinity)) }
+        end
+
+        context 'with 0 count_on_hand' do
+          before { stock_item.set_count_on_hand(0) }
+
+          it { is_expected.to eq(stock_location1.id => Spree::StockQuantities.new(variant => infinity)) }
+        end
+
+        context 'with negative count_on_hand' do
+          before { stock_item.set_count_on_hand(-1) }
+
+          it { is_expected.to eq(stock_location1.id => Spree::StockQuantities.new(variant => infinity)) }
+        end
+      end
+
+      context 'with soft-deleted stock_item' do
+        before { stock_item.discard }
+
+        it { is_expected.to eq({}) }
+      end
+
+      context 'with no stock_item' do
+        before { stock_item.destroy }
+
+        it { is_expected.to eq({}) }
       end
     end
   end

5fa71ff85fece29a7e3254b6dbc2588cdf337f4c
Giacomo Bertoldi <bertoldi.giacomo@gmail.com>
2021-04-19 13:57:49 +0200

    Add webhook handler for merchant revoked event
    Set seller status to revoked on MERCHANT.PARTNER-CONSENT.REVOKED event.

---
+++ app/models/solidus_paypal_marketplace/webhooks/handlers/merchant_partner_consent_revoked.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module SolidusPaypalMarketplace
+  module Webhooks
+    module Handlers
+      class MerchantPartnerConsentRevoked < Base
+        def call
+          if seller.update(status: :revoked)
+            { result: true }
+          else
+            { result: false, errors: seller.errors.full_messages }
+          end
+        end
+
+        private
+
+        def seller
+          Spree::Seller.find_by!(merchant_id_in_paypal: params["merchant_id"])
+        end
+      end
+    end
+  end
+end

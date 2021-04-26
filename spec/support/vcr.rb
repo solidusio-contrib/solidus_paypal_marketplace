54816967a1f1aaef86cc7bad03bc65aaf7b2340a
Giacomo Bertoldi <bertoldi.giacomo@gmail.com>
2021-04-26 17:49:45 +0200

    Update VCR configuration

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -16,6 +16,34 @@ VCR.configure do |config|
     interaction.request.headers['Authorization']&.first
   end
 
+  config.filter_sensitive_data('<Paypal Partner Attribution Id>') do |interaction|
+    interaction.request.headers['Paypal-Partner-Attribution-Id']&.first
+  end
+
+  config.register_request_matcher :generic_partner_referral do |request1, request2|
+    uri_without_ids1 = request1.uri
+                               .gsub(%r{partners/[^/]*}, "partners/filtered-id")
+                               .gsub(%r{merchant-integrations/.*}, "merchant-integrations/filtered-id")
+    uri_without_ids2 = request2.uri
+                               .gsub(%r{partners/[^/]*}, "partners/filtered-id")
+                               .gsub(%r{merchant-integrations/.*}, "merchant-integrations/filtered-id")
+    uri_without_ids1 == uri_without_ids2
+  end
+
+  config.after_http_request ->(request) {
+    true || request.uri.include?('/partners/') && request.uri.include?('/merchant-integrations/')
+  } do |request, _response|
+    request.uri.gsub!(%r{partners/[^/]*}, "partners/filtered-id")
+    request.uri.gsub!(%r{merchant-integrations/.*}, "merchant-integrations/filtered-id")
+  end
+
+  config.before_record(:paypal_api) do |interaction|
+    body = JSON.parse(interaction.response.body)
+    body["scope"] = 'FILTERED' if body["scope"]
+    interaction.response.body = body.slice("links", "scope", "payments_receivable")
+                                    .to_json
+  end
+
   # Let's you set default VCR record mode with VCR_RECORDE_MODE=all for re-recording
   # episodes. :once is VCR default
   record_mode = ENV.fetch('VCR_RECORD_MODE', :once).to_sym

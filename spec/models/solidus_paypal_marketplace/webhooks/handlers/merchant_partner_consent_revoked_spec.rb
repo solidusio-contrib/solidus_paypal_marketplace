95e89fbfefc653ae5c03d76e43cabe005d0589fc
Giacomo Bertoldi <bertoldi.giacomo@gmail.com>
2021-04-28 17:38:14 +0200

    Add webhook handler for payment capture completed

--- spec/models/solidus_paypal_marketplace/webhooks/handlers/merchant_partner_consent_revoked_spec.rb
+++ spec/models/solidus_paypal_marketplace/webhooks/handlers/merchant_partner_consent_revoked_spec.rb
@@ -9,38 +9,40 @@ RSpec.describe SolidusPaypalMarketplace::Webhooks::Handlers::MerchantPartnerCons
   let(:params) { { resource: { "merchant_id" => seller.merchant_id_in_paypal } } }
   let(:seller) { create(:seller, merchant_id_in_paypal: 'merchant-id') }
 
-  it do
-    expect(handler).to respond_to(:call).with(0).arguments
-  end
-
-  it do
-    expect(handler.call).to be_present
-  end
-
-  it do
-    expect(handler.call[:result]).to eq(true)
-  end
-
-  it do
-    expect { handler.call }.to change { seller.reload.status }.from('accepted').to('revoked')
-  end
-
-  context 'when the update fail' do
-    let(:errors) { OpenStruct.new(full_messages: ["generic error"]) }
-
-    before do
-      allow(Spree::Seller).to receive(:find_by).with(merchant_id_in_paypal: seller.merchant_id_in_paypal)
-                                               .and_return(seller)
-      allow(seller).to receive(:update).and_return(false)
-      allow(seller).to receive(:errors).and_return(errors)
+  describe '#call' do
+    it do
+      expect(handler).to respond_to(:call).with(0).arguments
     end
 
     it do
-      expect(handler.call[:result]).to eq(false)
+      expect(handler.call).to be_present
     end
 
     it do
-      expect(handler.call[:errors]).to eq(errors.full_messages)
+      expect(handler.call[:result]).to eq(true)
+    end
+
+    it do
+      expect { handler.call }.to change { seller.reload.status }.from('accepted').to('revoked')
+    end
+
+    context 'when the update fail' do
+      let(:errors) { OpenStruct.new(full_messages: ["generic error"]) }
+
+      before do
+        allow(Spree::Seller).to receive(:find_by).with(merchant_id_in_paypal: seller.merchant_id_in_paypal)
+                                                 .and_return(seller)
+        allow(seller).to receive(:update).and_return(false)
+        allow(seller).to receive(:errors).and_return(errors)
+      end
+
+      it do
+        expect(handler.call[:result]).to eq(false)
+      end
+
+      it do
+        expect(handler.call[:errors]).to eq(errors.full_messages)
+      end
     end
   end
 end

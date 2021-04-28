417c8b16ba79194de6969958ffb0c9321f75cc7c
Giacomo Bertoldi <bertoldi.giacomo@gmail.com>
2021-04-28 10:51:38 +0200

    Split Sdk spec in different tests for every method

--- spec/lib/solidus_paypal_marketplace/paypal_partner_sdk_generate_paypal_signup_link_spec.rb
+++ spec/lib/solidus_paypal_marketplace/paypal_partner_sdk_generate_paypal_signup_link_spec.rb
@@ -34,36 +34,5 @@ RSpec.describe SolidusPaypalMarketplace::PaypalPartnerSdk do
         expect(generate_paypal_sign_up_link).to eq link.href
       end
     end
-
-    context 'when PayPal returns an error' do
-      let(:paypal_client) { instance_double(PayPal::PayPalHttpClient) }
-      let(:paypal_http_error) do
-        PayPalHttp::HttpError.new(
-          'status_code',
-          OpenStruct.new(error: 'invalid_client', error_description: 'Client Authentication failed'),
-          'paypal-debug-id' => 'paypal-debug-id'
-        )
-      end
-      let(:rails_logger) { instance_double(ActiveSupport::Logger, error: nil) }
-
-      before do
-        allow(SolidusPaypalMarketplace).to receive(:client).and_return(paypal_client)
-        allow(paypal_client).to receive(:execute).and_raise(paypal_http_error)
-
-        allow(Rails).to receive(:logger).and_return(rails_logger)
-      end
-
-      it 'returns nil and logs the errors' do
-        expect(generate_paypal_sign_up_link).to be_nil
-
-        expect(rails_logger).to have_received(:error).with('status_code')
-        expect(rails_logger).to have_received(:error).with({ "paypal-debug-id" => "paypal-debug-id" })
-        expect(rails_logger).to have_received(:error).with(
-          OpenStruct.new(
-            error: "invalid_client", error_description: "Client Authentication failed"
-          )
-        )
-      end
-    end
   end
 end
